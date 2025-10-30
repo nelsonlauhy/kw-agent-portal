f0a46e05c3b32031e8f85fedca6005975c8723de
nelsonlauhy <nelson.lau.hy@outlook.com>
2025-10-30 15:26:16 -0400

    firebase setup

---
+++ assets/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCoxLx41UHtToiQwpJZIEXUjKbjJW-eN4A",
+  authDomain: "kw-agent-portal.firebaseapp.com",
+  projectId: "kw-agent-portal",
+  storageBucket: "kw-agent-portal.firebasestorage.app",
+  messagingSenderId: "355575468285",
+  appId: "1:355575468285:web:c5436f16c51accd8c72e94",
+  measurementId: "G-7HRWRLT6PV"
+};
